d6b3f0ffeb8b162c0334a4e3c9be61e43910cfa7
jacekdc <59376989+jacekdc@users.noreply.github.com>
2020-08-03 21:14:03 +0200

    2.0
    2.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# ytmp3py
+
+simple youtube mp3 download script
+
+- Paste ursl into links.txt
+- Write song names to names.txt
+
+Run yt.py and follow instructions :)
+
+List of used additional libraries and install links:
+
+pip3 install youtube_search
+pip3 install moviepy.editor
+pip3 install youtube-dl
--- yt.py
+++ yt.py
@@ -5,9 +5,19 @@ import json
 from youtube_search import YoutubeSearch
 from moviepy.editor import *
 
+numsearch = 1
+link = ""
+fo = "0"
+
 def download(line):
     try:
         os.system('youtube-dl "{}" --no-check-certificate'.format(line))
+        convert('mp4')
+        convert('webm')
+        convert('mkv')
+        remove('mp4')
+        remove('webm')
+        remove('mkv')
     except:
         print("Could not download that video")
             
@@ -22,40 +32,54 @@ def list():
             line = fp.readline()
             cnt += 1
             
-def convert():
-    print("\n* Converting to mp3 *\n")
-    for file in glob.glob("*.mp4"):
+def names():
+    filepath = "names.txt"
+    with open(filepath) as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            search(line.strip())
+            line = fp.readline()
+            cnt += 1
+            
+def convert(fmt):    
+    for file in glob.glob("*."+fmt):
         video = VideoFileClip(file)
-        video.audio.write_audiofile("mp3/" + file.replace("mp4","") +".mp3")
+        video.audio.write_audiofile("mp3/" + file.replace(fmt,"") +".mp3")
         
-def remove():
-    for file in glob.glob("*.mp4"):
+def remove(fmt):
+    for file in glob.glob("*."+fmt):
         os.remove(file)       
+
+def search(name):
+    count = 1
+    results = YoutubeSearch(name, max_results=numsearch).to_json()
+    data = json.loads(results)
+    for videos in data['videos']:
+        link = 'https://www.youtube.com'+videos['url_suffix']
+        download(link)
+        count += 1
         
-def search():
-    print("Keywords")
-    sr = input()
-    results = YoutubeSearch(sr, max_results=10).to_json()
-    print(results)
-     
+        
 def start():
-    print("What do you want to do?\n1. Download & remove mp4 files\n2. Download list.txt\n3. Remove mp4 files\n4. Exit")
+    print("What do you want to do?\n1. Use links.txt to download mp3\n2. Use names.txt to search and download mp3\n3 NUMBER to set how many found youtube videos download. FX. 3 5 or 3 10\nEnter. to finish\n")
     op = input()
     if op == "1":
         list()
-        convert()
-        remove()
         start()
     if op == "2":
-        list()
-        convert()
+        names()
         start()
-    if op == "3":
-        remove()
-        start()
-    if op == "4":
-        exit()
+    try:
+        if op[0] == "3":
+            numsearch = op[2:5]
+            print("\n* you will download "+op[2:5]+" songs *\n")
+            start()
+    except:
+        sys.exit()
+     
+if fo == "0":
+    start()
+    fo = "1"
     
-start()
-    
-print("\n* Finished *")
+print("* Finished *")
