4673f2f9f1b9151c3236be3d1fc15f4fb821a48d
jacekdc <59376989+jacekdc@users.noreply.github.com>
2020-08-06 19:15:32 +0200

    updated quality of code
    updated quality of code

--- yt.py
+++ yt.py
@@ -8,48 +8,41 @@ from moviepy.editor import *
 numsearch = 1
 link = ""
 fo = "0"
+vf = ['mp4','mkv','webm']
 
 def download(line):
     try:
         os.system('youtube-dl "{}" --no-check-certificate'.format(line))
-        convert('mp4')
-        convert('webm')
-        convert('mkv')
-        remove('mp4')
-        remove('webm')
-        remove('mkv')
+        convert(vf)
+        remove(vf)
     except:
         print("Could not download that video")
             
-def list():
-    filepath = "links.txt"
+            
+def look(name, mode):
+    filepath = name
     with open(filepath) as fp:
         line = fp.readline()
         cnt = 1
         while line:
             print("Line {}: {}".format(cnt, line.strip()))
-            download(line)
+            if mode == "link":
+                download(line)
+            if mode == "name":
+                search(line.strip())
             line = fp.readline()
             cnt += 1
             
-def names():
-    filepath = "names.txt"
-    with open(filepath) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            search(line.strip())
-            line = fp.readline()
-            cnt += 1
-            
-def convert(fmt):    
-    for file in glob.glob("*."+fmt):
-        video = VideoFileClip(file)
-        video.audio.write_audiofile("mp3/" + file.replace(fmt,"") +".mp3")
+def convert(fmt):
+    for x in range(len(fmt)):
+        for file in glob.glob("*."+fmt[x]):
+            video = VideoFileClip(file)
+            video.audio.write_audiofile("mp3/" + file.replace(fmt[x],"") +".mp3")
         
 def remove(fmt):
-    for file in glob.glob("*."+fmt):
-        os.remove(file)       
+    for x in range(len(fmt)):
+        for file in glob.glob("*."+fmt[x]):
+            os.remove(file)       
 
 def search(name):
     count = 1
@@ -65,10 +58,10 @@ def start():
     print("What do you want to do?\n1. Use links.txt to download mp3\n2. Use names.txt to search and download mp3\n3 NUMBER to set how many found youtube videos download. FX. 3 5 or 3 10\nEnter. to finish\n")
     op = input()
     if op == "1":
-        list()
+        look("links.txt","link")
         start()
     if op == "2":
-        names()
+        look("names.txt","name")
         start()
     try:
         if op[0] == "3":
